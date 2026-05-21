b8b4b83409426c61b5724c19c02ed0d80a2ca497
Harisnow-bear <hari5262@gmail.com>
2026-05-21 17:51:35 +0530

    "initial commit"

---
+++ tests/workshop/demo_1/try.spec.ts
@@ -0,0 +1,33 @@
+import {test} from '@playwright/test';
+
+test('Page home', async ({page}) => {
+await page.goto('/');
+await page.waitForTimeout(5000);
+await page.reload();
+});
+
+test.only('Free Trial',async ({page})=> {
+await page.goto('/');
+// await page.click('[data-backend-test-id="close-button"]');
+await page.locator('[data-ga-name="free trial"]').first().click();
+// await page.waitForTimeout(5000);
+await page.locator('#new_user_first_name').fill('John');
+await page.locator('#new_user_last_name').fill('Snow');
+await page.getByTestId('new-user-username-field').fill('johnsnow');
+await page.getByTestId('new-user-email-field').fill('john.snow@example.com');
+await page.locator('#new_user_password').fill('password123');
+
+const checkbox =  page.locator('#new_user_onboarding_status_email_opt_in');
+
+if(await checkbox.isChecked()) {
+    console.log('Checkbox is already checked');
+}else{
+    await checkbox.check();
+}
+});
+
+
+
+
+
+
---
+++ tests/workshop/forms/forms.spec.ts
@@ -0,0 +1,54 @@
+import {expect, test} from '@playwright/test';
+
+
+
+
+test.skip('Forms submission application',async ({page})=>{
+
+    await page.goto('https://demo.playwright.dev/todomvc');
+
+   const inputBox =  page.getByPlaceholder('What needs to be done?');
+    await inputBox.fill('Buy Milk');
+    await inputBox.press('Enter');
+    await inputBox.fill('Buy Coffee Mix')
+    await inputBox.press('Enter');
+    await page.waitForTimeout(3000);
+
+    await page.locator('.toggle').nth(0).check();
+
+    const todoItem = page.getByTestId('todo-item').nth(0);
+
+    await expect(todoItem).toHaveClass('completed');
+
+    
+});
+
+
+test.skip ('Form Handling remove item',  async ({page})=> {
+ await page.goto('https://demo.playwright.dev/todomvc');
+
+ const placeholder ='//input[@placeholder="What needs to be done?"]';
+    await page.fill(placeholder,'Batmobile');
+    await page.press(placeholder,'Enter');
+    await page.fill(placeholder,'Batman Suit');
+    await page.press(placeholder,'Enter');
+    await page.waitForTimeout(3000);
+    const iterm1 =  page.getByTestId('todo-item').nth(1);
+    await iterm1.hover();
+    await page.locator('.destroy').nth(1).click();
+
+    await expect(iterm1).not.toBeVisible();
+    
+
+
+});
+
+
+test.only('get title of the page', async ({page})=> {
+    await page.goto('https://demo.playwright.dev/todomvc');
+    const title = await page.title();
+    console.log('Title of the page is : ' +  title);
+    await page.waitForTimeout(3000);
+});
+
+
